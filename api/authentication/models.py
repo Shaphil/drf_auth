f9f4d1bddfac2a48682075c2f8749ef2f7da44aa
Shaphil <shaphil2025@gmail.com>
2019-04-07 17:58:02 +0600

    Add models AbstractBaseUser, User and Token

--- api/authentication/models.py
+++ api/authentication/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.contrib.auth.hashers import (
+    check_password, make_password,
+)
+from django.utils import timezone
 
-# Create your models here.
+
+class AbstractBaseUser(models.Model):
+    password = models.CharField('password', max_length=128)
+    last_login = models.DateTimeField('last login', blank=True, null=True)
+
+    # Stores the raw password if set_password() is called so that it can
+    # be passed to password_changed() after the model is saved.
+    _password = None
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.set_password(self.password)
+        super().save(*args, **kwargs)
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+        self._password = raw_password
+
+    def check_password(self, raw_password):
+        """
+        Return a boolean of whether the raw_password was correct. Handles
+        hashing formats behind the scenes.
+        """
+        def setter(raw_password):
+            self.set_password(raw_password)
+            # Password hash upgrades shouldn't be considered password changes.
+            self._password = None
+            self.save(update_fields=["password"])
+        return check_password(raw_password, self.password, setter)
+
+
+class User(AbstractBaseUser):
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        error_messages={
+            'unique': "A user with that username already exists.",
+        },
+    )
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(blank=True)
+    is_active = models.BooleanField(
+        'active',
+        default=True,
+        help_text='Designates whether this user should be treated as active. '
+        'Unselect this instead of deleting accounts.',
+    )
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.username
+
+
+class Token(models.Model):
+    """
+    The default authorization token model.
+    """
+    key = models.CharField(max_length=40, primary_key=True)
+    user = models.OneToOneField(
+        User, related_name='auth_token',
+        on_delete=models.CASCADE, verbose_name="User"
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.set_key()
+        return super(Token, self).save(*args, **kwargs)
+
+    def set_key(self):
+        if not self.key:
+            self.key = self.generate_key()
+
+    def generate_key(self):
+        return binascii.hexlify(os.urandom(20)).decode()
+
+    def __str__(self):
+        return self.key
