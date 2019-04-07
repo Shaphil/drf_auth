df78e97b3984c110cfcfad734eb251eba182a899
Shaphil <shaphil2025@gmail.com>
2019-04-07 19:15:15 +0600

    Add url rule for RegistrationView

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import Default, UserViewSet
+from .views import Default, UserViewSet, RegistrationView
 
 
 router = routers.DefaultRouter()
@@ -10,4 +10,5 @@ router.register('users', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('greet/', Default.as_view()),
+    path('register/', RegistrationView.as_view(), name='registration'),
 ]
