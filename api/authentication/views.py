a541bff6a757b6908f6351b152ed54aea4ad7fe6
Shaphil <shaphil2025@gmail.com>
2019-04-07 18:57:45 +0600

    Add UserViewSet

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -1,5 +1,8 @@
-from rest_framework import status, views
+from rest_framework import status, views, viewsets
 from rest_framework.response import Response
+
+from .models import User
+from .serializers import UserSerializer
 
 
 class Default(views.APIView):
@@ -10,3 +13,8 @@ class Default(views.APIView):
     def get(self, request, *args, **kwargs):
         message = {'message': 'Welcome to the Authentication API'}
         return Response(message, status=status.HTTP_200_OK)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
