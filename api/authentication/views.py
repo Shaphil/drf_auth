fa55ff06abd0c9b9f8f04ef9bcdf6ef4582de380
Shaphil <shaphil2025@gmail.com>
2019-04-07 19:14:45 +0600

    Add RegistrationView to views

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -10,6 +10,8 @@ class Default(views.APIView):
     Dummy view
     """
 
+    authentication_classes = ()
+
     def get(self, request, *args, **kwargs):
         message = {'message': 'Welcome to the Authentication API'}
         return Response(message, status=status.HTTP_200_OK)
@@ -18,3 +20,30 @@ class Default(views.APIView):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class RegistrationView(views.APIView):
+    authentication_classes = ()
+
+    def post(self, request, *args, **kwargs):
+        required_fields = ('username', 'email', 'password')
+        for field in required_fields:
+            if field not in request.data:
+                return Response(
+                    {'error': 'This field is required {}'.format(field)},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+        try:
+            user = User.objects.get(username=request.data['username'])
+        except User.DoesNotExist:
+            user = User(
+                username=request.data['username'],
+                email=request.data['email'],
+                password=request.data['password']
+            )
+            user.save()
+
+            return Response({'Message': 'User created'}, status=status.HTTP_201_CREATED)
+
+        return Response({'Error': 'Username already taken'}, status=status.HTTP_409_CONFLICT)
