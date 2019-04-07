475a80fd71b23416a30e1ce41019b4316e428e1f
Shaphil <shaphil2025@gmail.com>
2019-04-07 19:13:43 +0600

    Add default authentication class setting

--- api/api/settings.py
+++ api/api/settings.py
@@ -121,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Custom settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'authentication.auth.TokenBasedAuthentication',
+    )
+}
